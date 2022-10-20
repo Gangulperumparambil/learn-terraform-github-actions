da6a12d83521b20985934ab60079e2fc7faf6298
Gangulperumparambil <gangul.perumparambil@sreyo.com>
2022-10-20 10:39:43 -0400

    Newcommit

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "****"
+    organization = "gangul"
 
     workspaces {
-      name = "****"
+      name = "learn-terraform"
     }
   }
 }
